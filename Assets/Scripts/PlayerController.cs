0a7776b912386054cb01514979c94a80e32a44b7
Tom Lengkeek <330713@student.mboutrecht.nl>
2021-02-03 11:00:50 +0100

    Added player controller
    type: addition
    change: added player controller script
    why: player needs movement to play game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10f;
+    public float xRange = 10f;
+    public float zRangeDown = -8f;
+    public float zRangeUp = -1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get input from the player
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        //prevent the player from going out of bounds
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z > zRangeUp)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeUp);
+        }
+        else if (transform.position.z < zRangeDown)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeDown);
+        }
+
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
+
+            
+
+
+
+    }
+}
